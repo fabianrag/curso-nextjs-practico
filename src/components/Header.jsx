02272440892ccdefb1c695c6d9e52253d0a1b40c
Fabian Araya <fbnarayaguerra@gmail.com>
2022-10-23 12:55:52 -0300

    3 commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import Link from 'next/link'
 import Image from 'next/image'
 import Menu from '@components/Menu'
 import MyOrder from '@containers/MyOrder'
@@ -16,7 +17,9 @@ const Header = () => {
       <nav className={styles.Nav}>
         <Image src={menu} alt='menu' className={styles.menu} />
         <div className={styles['navbar-left']}>
-          <Image src={logo} alt='logo' className={styles['nav-logo']} />
+          <Link href='/'>
+            <Image src={logo} alt='logo' className={styles['nav-logo']} />
+          </Link>
           <ul>
             <li>
               <a href='/'>All</a>
@@ -46,7 +49,10 @@ const Header = () => {
             >
               platzi@example.com
             </li>
-            <li className='navbar-shopping-cart' onClick={() => toggleOrder()}>
+            <li
+              className={styles['navbar-shopping-cart']}
+              onClick={() => toggleOrder()}
+            >
               <Image
                 className={(styles['more-clickable-area'], styles['pointer'])}
                 src={shoppingCart}
